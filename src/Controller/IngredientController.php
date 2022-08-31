1770c2a18c37fd3640ad4653ef3620ec2d3ec7f0
Mana444 <manarii.bremond2022@campus-eni.fr>
2022-08-31 02:34:33 +0200

    Pagination knp OK

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,19 +3,26 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IngredientController extends AbstractController
 {
     /**Injection de dépendance du Repository d'ingredient dans la function index pour aller chercher des données en DB avec  FindAll()
-     **/
+     *fonction pour Display tous les ingredients en db
+     */
     #[Route('/ingredient', name: 'app_ingredient')]
-    public function index(IngredientRepository $repository): Response
+    public function index(IngredientRepository $repository,PaginatorInterface $paginator,Request $request): Response
     {
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page',1),10);
+
         return $this->render('pages/ingredient/index.html.twig', [
-            'ingredients' => $repository->findAll()
+            'ingredients' => $ingredients
         ]);
     }
 }
